da886717218361fdb3ad5bb5e86db4d13c6e1cac
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-02 16:40:04 -0300

    C# - 1002 - Accepted

---
+++ C#/Beginner/1002/1002/Program.cs
@@ -0,0 +1,8 @@
+﻿
+const double pi = 3.14159; 
+
+double radius = Convert.ToDouble(Console.ReadLine());
+
+double area = pi * Math.Pow(radius, 2);
+
+Console.WriteLine("A=" + string.Format("{0:0.0000}", area));
