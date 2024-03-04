59b09c6b2cf58cbafb27482bb0b699fb00f793c4
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-04 00:14:36 -0300

    C# - 1005 - Accepted

---
+++ C#/Beginner/1005/1005/Program.cs
@@ -0,0 +1,9 @@
+﻿
+double A, B, average;
+
+A = Convert.ToDouble(Console.ReadLine());
+B = Convert.ToDouble(Console.ReadLine());
+
+average = ((A * 3.5) + (B * 7.5)) / 11.0 ;
+
+Console.WriteLine("MEDIA = " + string.Format("{0:0.00000}", average));
