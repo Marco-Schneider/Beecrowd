fea8a7fe97ba6bc490664cc95c8b229299e90051
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-04 00:20:53 -0300

    C# - 1007 - Accepted

---
+++ C#/Beginner/1007/1007/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int A, B, C, D;
+int DIFERENCA;
+
+A = Convert.ToInt32(Console.ReadLine());
+B = Convert.ToInt32(Console.ReadLine());
+C = Convert.ToInt32(Console.ReadLine());
+D = Convert.ToInt32(Console.ReadLine());
+
+DIFERENCA = (A * B - C * D);
+
+Console.WriteLine("DIFERENCA = " + DIFERENCA);
