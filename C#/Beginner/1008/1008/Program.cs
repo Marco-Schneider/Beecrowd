a651b36d99eb1871c5aaf280e4486c00b85c7fc0
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-04 00:27:32 -0300

    C# - 1008 - Accepted

---
+++ C#/Beginner/1008/1008/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int employeeNumber, workedHours;
+double paymentPerHour, salary;
+
+employeeNumber = Convert.ToInt32(Console.ReadLine());
+workedHours = Convert.ToInt32(Console.ReadLine());
+paymentPerHour = Convert.ToDouble(Console.ReadLine());
+
+salary = workedHours * paymentPerHour;
+
+Console.WriteLine("NUMBER = " + employeeNumber);
+Console.WriteLine("SALARY = U$ " + string.Format("{0:0.00}", salary));
