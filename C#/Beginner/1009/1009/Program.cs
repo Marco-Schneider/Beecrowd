7ae21b586d05dfd39170a324b62c1c5fffb43927
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-06 21:29:40 -0300

    C# - 1009 - Accepted

---
+++ C#/Beginner/1009/1009/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string employeeName = "";
+double baseSalary, sales, salary;
+
+employeeName = Console.ReadLine();
+baseSalary = Convert.ToDouble(Console.ReadLine());
+sales = Convert.ToDouble(Console.ReadLine());
+
+salary = baseSalary + sales * 0.15;
+
+Console.WriteLine("TOTAL = R$ " + string.Format("{0:0.00}", salary));
