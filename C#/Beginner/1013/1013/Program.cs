374e8358324cb009fa505224b115c6a7f69c50c2
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-08 18:34:23 -0300

    C# - 1013 - Accepted

---
+++ C#/Beginner/1013/1013/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int A, B, C;
+
+string[] input = Console.ReadLine().Split(' ');
+
+A = Convert.ToInt32(input[0]);
+B = Convert.ToInt32(input[1]);
+C = Convert.ToInt32(input[2]);
+
+int greaterAB, greaterABC;
+
+greaterAB = (A + B + Math.Abs(A - B)) / 2;
+greaterABC = (greaterAB + C + Math.Abs(greaterAB - C)) / 2;
+
+Console.WriteLine(greaterABC + " eh o maior");
