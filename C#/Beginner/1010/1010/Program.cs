63c1db2d29ce5f028f41e4b18da831517ff2095b
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-08 00:48:54 -0300

    C# - 1010 - Accepted

---
+++ C#/Beginner/1010/1010/Program.cs
@@ -0,0 +1,15 @@
+﻿
+const int numberOfEntries = 2;
+double totalPrice = 0;
+
+for (int i = 0; i < numberOfEntries; i++)
+{
+    string[] input = Console.ReadLine().Split(' ');
+
+    int quantity = Convert.ToInt32(input[1]);
+    double unitPrice = Convert.ToDouble(input[2]);
+
+    totalPrice = totalPrice + quantity * unitPrice;
+}
+
+Console.WriteLine("VALOR A PAGAR: R$ " + string.Format("{0:0.00}", totalPrice));
