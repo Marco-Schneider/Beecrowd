d59e09d781829c241743d05c79fe5612e97aefc0
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-08 18:28:44 -0300

    C# - 1012 - Accepted

---
+++ C#/Beginner/1012/1012/Program.cs
@@ -0,0 +1,23 @@
+﻿
+const double pi = 3.14159;
+double A, B, C;
+
+string[] input = Console.ReadLine().Split(' ');
+
+A = Convert.ToDouble(input[0]);
+B = Convert.ToDouble(input[1]);
+C = Convert.ToDouble(input[2]);
+
+double triangleArea, circleArea, trapezeArea, squareArea, rectangleArea;
+
+triangleArea = (A * C) / 2.0;
+circleArea = pi * Math.Pow(C, 2);
+trapezeArea = (A + B) * (C / 2.0);
+squareArea = Math.Pow(B, 2);
+rectangleArea = A * B;
+
+Console.WriteLine("TRIANGULO: " + string.Format("{0:0.000}", triangleArea));
+Console.WriteLine("CIRCULO: " + string.Format("{0:0.000}", circleArea));
+Console.WriteLine("TRAPEZIO: " + string.Format("{0:0.000}", trapezeArea));
+Console.WriteLine("QUADRADO: " + string.Format("{0:0.000}", squareArea));
+Console.WriteLine("RETANGULO: " + string.Format("{0:0.000}", rectangleArea));
