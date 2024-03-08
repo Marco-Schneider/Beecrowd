ebc47f9a3f2715d7dc119fec12dbe16ec7247cb3
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-08 18:11:29 -0300

    C# - 1011 - Accepted

---
+++ C#/Beginner/1011/1011/Program.cs
@@ -0,0 +1,9 @@
+﻿
+const double pi = 3.14159;
+double radius, volume;
+
+radius = Convert.ToDouble(Console.ReadLine());
+
+volume = (4/3.0) * pi * Math.Pow(radius, 3);
+
+Console.WriteLine("VOLUME = " + string.Format("{0:0.000}", volume));
