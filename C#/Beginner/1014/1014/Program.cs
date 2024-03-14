8d9d53439d775dc6a500ec8943f83ee33aa7c165
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-14 20:09:25 -0300

    C# - 1014 - Accepted

---
+++ C#/Beginner/1014/1014/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int travelledDistance;
+double amountOfFuelUsed;
+
+double vehicleAutonomy;
+
+travelledDistance = Convert.ToInt32(Console.ReadLine());
+amountOfFuelUsed = Convert.ToDouble(Console.ReadLine());
+
+vehicleAutonomy = travelledDistance / amountOfFuelUsed;
+
+Console.WriteLine(string.Format("{0:0.000}", vehicleAutonomy) + " km/l");
